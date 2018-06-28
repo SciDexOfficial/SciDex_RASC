7ee88a77e6efb5ccfa0edc88b983410e2cbb2a42
Yaschenko <yura.yaschenko@gmail.com>
2018-06-28 11:27:36 +0300

    updated truffel.js

--- truffel/truffle.js
+++ truffel/truffle.js
@@ -1,18 +1,26 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+const mnemonic = process.env.MNEMONIC
+const fromAddress = process.env.FROM_ADDRESS
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8501,
+      network_id: "*", 
+      gasprice: 1
+    },
+    rinkeby: {
+      provider : () => new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/3vj9Xnohf6SSZo69i0aH"), // TODO: change with our own node addres
+      from: fromAddress.toLowerCase(),
+      network_id: "4",
+      gas: 6712390
+    },   
+    main: {
+      provider : () => new HDWalletProvider(mnemonic, "https://mainnet.infura.io/3vj9Xnohf6SSZo69i0aH"), // TODO: change with our own node addres
+      from: fromAddress.toLowerCase(),
+      network_id: "1",
+    }
+  }
 };
