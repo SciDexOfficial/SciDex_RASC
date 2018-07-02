644cca5e3510aa7b9660ffa51cec1724544ee530
Yaschenko <yura.yaschenko@gmail.com>
2018-07-02 15:02:27 +0300

    updated profile data storing

--- truffel/contracts/RASC_Store.sol
+++ truffel/contracts/RASC_Store.sol
@@ -6,33 +6,35 @@ import "./RASC_Transaction.sol";
 
 contract RASC_Store is RASC_Transaction, RASC_User {
     using SafeMath for uint;
-    // mapping(uint => uint) itemsHolders;
+
+    //return all items availabel for sender
     function getItemsGroupIds() public view returns(uint[] memory ids) {
         uint groupsCount = itemsGroups.length;
         ids = new uint[](groupsCount);
         uint groupsFilteringCount = 0;
-        User memory user = getUser(msg.sender);
+        uint userIndex = getUserIndex(msg.sender);
         for (uint i = 0; i < groupsCount; i++) {
             ItemsGroup memory group = itemsGroups[i];
 
-            if (hasAccess(user, group) == true){
+            if (hasAccess(userIndex, group) == true){
                 ids[groupsFilteringCount] = i;
                 groupsFilteringCount++;
             }
         }
     }
-    function hasAccess(User memory user, ItemsGroup memory group) internal pure returns(bool) {
+    //check if user has access to itemsGroup
+    function hasAccess(uint userIndex, ItemsGroup memory group) internal view returns(bool) {
         uint accessCount = group.access.length;
         for (uint i = 0; i < accessCount; i++){
-            if (checkUserAccess(user, group.access[i]) == false){
+            if (checkUserAccess(userIndex, group.access[i]) == false){
                 return false;
             }
         }
         return true;
     }
-    function checkUserAccess(User memory user, BasicAccess memory access) internal pure returns(bool) {
-        
-        uint value = getValueForAccess(user, access.accessType).mul(access.multiplier);
+    //
+    function checkUserAccess(uint userIndex, BasicAccess memory access) internal view returns(bool) {
+        uint value = getValueForAccessType(userIndex, access.accessType).mul(access.multiplier);
         return ((value >= access.minValue) && (value <= access.maxValue));
         
     }    
--- truffel/contracts/RASC_User.sol
+++ truffel/contracts/RASC_User.sol
@@ -6,14 +6,43 @@ contract RASC_User {
         string name;
         address wallet;
     }
+    //all registered users
     User[] users;
+
+    //key0:user index
+    //key1:type of the field
+    //value:field's value
     mapping(uint => mapping(uint => uint)) usersFields;
-    mapping(address => uint) usersIds;
+    
+    //key: user address 
+    //value: index of user in the users array
+    mapping(address => uint) usersIndexies;
+    
+    //return user from address if exist
     function getUser(address userAddress) internal view returns(User) {
-        uint userId = usersIds[userAddress];
+        uint userId = getUserIndex(userAddress);
         return users[userId];
     }
-    function getValueForAccess(uint userId, uint accessType) internal view returns(uint) {
-        return usersFields[userId][accessType];
+    
+    //converting user address to user index
+    function getUserIndex(address userAddress) internal view returns(uint) {
+        uint userId = usersIndexies[userAddress];
+        //check if user exist
+        require(users.length > userId);
+        return userId;
+    }
+    //get value for current field type
+    function getValueForAccessType(uint userId, uint fieldType) internal view returns(uint) {
+        return usersFields[userId][fieldType];
+    }
+    //updating profile data
+    function updateMyData(uint[] memory types, uint[] memory values) public {
+        require(types.length == values.length);
+        uint userIndex = getUserIndex(msg.sender);
+        
+        uint typesCount = types.length;
+        for (uint i = 0; i < typesCount; i++) {
+            usersFields[userIndex][types[i]] = values[i];
+        }
     }
 }
