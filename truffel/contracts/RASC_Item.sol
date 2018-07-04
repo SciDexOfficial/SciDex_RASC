1cddc6c14f7316dddc7916ae9d9afc8ae5423be2
Yaschenko <yura.yaschenko@gmail.com>
2018-07-04 16:07:36 +0300

    updated item structure

--- truffel/contracts/RASC_Item.sol
+++ truffel/contracts/RASC_Item.sol
@@ -3,9 +3,16 @@ pragma solidity ^0.4.23;
 contract RASC_Item {
     //basic data item
     struct Item {
-        address owner;
-        bytes data;
+        string data;
         uint price;
     }
     Item[] items;
+    
+    mapping (uint => address[]) itemsOwners;
+
+    function createItem(string data, uint price) public returns(uint index) {
+        Item memory item = Item(data, price);
+        index = items.push(item) - 1;
+        itemsOwners[index].push(msg.sender);
+    }
 }
