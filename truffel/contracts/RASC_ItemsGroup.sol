e7923f0e1f61b71ff922e4d85d48e42aec02d434
Yaschenko <yura.yaschenko@gmail.com>
2018-07-05 16:59:20 +0300

    implemented access control methonds

--- truffel/contracts/RASC_ItemsGroup.sol
+++ truffel/contracts/RASC_ItemsGroup.sol
@@ -13,6 +13,12 @@ contract RASC_ItemsGroup is RASC_Item, RASC_Access {
     mapping(uint => uint[]) groupsItems;
     mapping(uint => BasicAccess[]) groupsAccess;
 
+    modifier onlyGroupOwner(uint groupIndex) {
+        ItemsGroup memory group = itemsGroups[groupIndex];
+        require(group.owner == msg.sender);
+        _;
+    } 
+
     function getItemsGroup(uint index) internal view returns(ItemsGroup) {
         return itemsGroups[index];
     }
@@ -27,23 +33,29 @@ contract RASC_ItemsGroup is RASC_Item, RASC_Access {
         }
         
     }
-    function updateGroup(uint index, uint price) public {
-
+    function updateGroup(uint index, uint price) public onlyGroupOwner(index) {
+        ItemsGroup storage group = itemsGroups[index];
+        group.price = price;
     }
-    function addGroupAccess() {
-
+    function addGroupAccess(uint groupIndex, uint fieldType, uint minValue, uint maxValue, uint multiplier) public onlyGroupOwner(groupIndex) {
+        BasicAccess memory access = BasicAccess(fieldType, minValue, maxValue, multiplier);
+        groupsAccess[groupIndex].push(access);
     }
-    function deleteAccess(uint groupIndex) {
-
+    function deleteAccess(uint groupIndex) public onlyGroupOwner(groupIndex) {
+        //remove all access objects
     }
-    function updateAccess(uint groupIndex, uint accessIndex) {
-
+    function updateAccess(uint groupIndex, uint accessIndex, uint fieldType, uint minValue, uint maxValue, uint multiplier) public onlyGroupOwner(groupIndex) {
+        BasicAccess storage access = groupsAccess[groupIndex][accessIndex];
+        access.userFieldType = fieldType;
+        access.minValue = minValue;
+        access.maxValue = maxValue;
+        access.multiplier = multiplier;
     }
-    function getGroupAccessCount(uint index) public view returns(uint) {
+    function getGroupAccessCount(uint index) public view onlyGroupOwner(index) returns(uint) {
         return groupsAccess[index].length;
     }
-    
-    function getGroupAccess(uint groupIndex, uint accessIndex) public view returns(uint, uint, uint , uint) {
+
+    function getGroupAccess(uint groupIndex, uint accessIndex) public view onlyGroupOwner(groupIndex) returns(uint, uint, uint , uint) {
         BasicAccess memory access = groupsAccess[groupIndex][accessIndex];
         return (access.userFieldType, access.minValue, access.maxValue, access.multiplier);
     }
