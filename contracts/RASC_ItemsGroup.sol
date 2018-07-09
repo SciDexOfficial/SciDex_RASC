9df06af7dacaa96d283f32477579328bdc1f2240
Yaschenko <yura.yaschenko@gmail.com>
2018-07-09 15:31:23 +0300

    updated events

--- contracts/RASC_ItemsGroup.sol
+++ contracts/RASC_ItemsGroup.sol
@@ -4,6 +4,10 @@ import "./RASC_Item.sol";
 import "./RASC_Access.sol";
 
 contract RASC_ItemsGroup is RASC_Item, RASC_Access {
+    //events
+    event GroupCreated(uint groupIndex);
+    event GroupUpdated(uint groupIndex);
+    event GroupAccessUpdated(uint groupIndex);
     //group of items for selling
     struct ItemsGroup {
         uint price;
@@ -31,25 +35,36 @@ contract RASC_ItemsGroup is RASC_Item, RASC_Access {
         for (uint i = 0; i < items.length; i++) {
             groupsItems[index].push(items[i]);
         }
-        
+        emit GroupCreated(index);
     }
     function updateGroup(uint index, uint price) public onlyGroupOwner(index) {
         ItemsGroup storage group = itemsGroups[index];
         group.price = price;
+
+        emit GroupUpdated(index);
     }
     function addGroupAccess(uint groupIndex, uint fieldType, uint minValue, uint maxValue, uint multiplier) public onlyGroupOwner(groupIndex) {
         BasicAccess memory access = BasicAccess(fieldType, minValue, maxValue, multiplier);
         groupsAccess[groupIndex].push(access);
     }
-    function deleteAccess(uint groupIndex) public view onlyGroupOwner(groupIndex) {
+    function deleteAccess(uint groupIndex) public onlyGroupOwner(groupIndex) {
+        delete groupsAccess[groupIndex];
         //remove all access objects
     }
-    function updateAccess(uint groupIndex, uint accessIndex, uint fieldType, uint minValue, uint maxValue, uint multiplier) public onlyGroupOwner(groupIndex) {
+    function updateAccess(
+        uint groupIndex, 
+        uint accessIndex, 
+        uint fieldType, 
+        uint minValue, 
+        uint maxValue, 
+        uint multiplier) public onlyGroupOwner(groupIndex) {
         BasicAccess storage access = groupsAccess[groupIndex][accessIndex];
         access.userFieldType = fieldType;
         access.minValue = minValue;
         access.maxValue = maxValue;
         access.multiplier = multiplier;
+
+        emit GroupAccessUpdated(groupIndex);
     }
     function getGroupAccessCount(uint index) public view onlyGroupOwner(index) returns(uint) {
         return groupsAccess[index].length;
