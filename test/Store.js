c29f0e2534f5e86607b850b808994cd2b70e7c0a
Yaschenko <yura.yaschenko@gmail.com>
2018-07-30 12:34:14 +0300

    added purchase test

--- test/Store.js
+++ test/Store.js
@@ -9,6 +9,14 @@ contract("Testing Store contract", async (accounts) => {
         
         assert.equal(itemsCount.toNumber(), itemsCountBefore.toNumber() + 1, "cannot create item")
     })
+    it("create second item test", async() => {
+        let instance = await Store.deployed()
+        let itemsCountBefore = await instance.getItemsCount.call()
+        await instance.createItem("http://test2/link/", 12 * 2, {from: accounts[3]})
+        let itemsCount = await instance.getItemsCount.call()
+        
+        assert.equal(itemsCount.toNumber(), itemsCountBefore.toNumber() + 1, "cannot create item")
+    })
     it("get item test", async() => {
         let instance = await Store.deployed()
         let info = await instance.getItemInfo.call(0)
@@ -19,5 +27,16 @@ contract("Testing Store contract", async (accounts) => {
         let instance = await Store.deployed()
         let items = await instance.getCreatedItems.call()
         assert.equal(items[0].toNumber(), 0, "incorrect value " + items[0].toNumber())
+    })
+    it("get item price test", async() => {
+        let instance = await Store.deployed()
+        let price = await instance.getItemPrice.call("0", [], [])
+        assert.equal(price.toNumber(), 12, "incorrect price " + price.toNumber())
+    })
+    it("buy item test", async() => {
+        let instance = await Store.deployed()
+        await instance.buyItem(1, [], [], {from: accounts[1], value: 30})
+        let items = await instance.getBoughtItems.call({from: accounts[1]})
+        assert.equal(items[0].toNumber(), 1, "incorrect value " + items[0].toNumber())
     })
 })
