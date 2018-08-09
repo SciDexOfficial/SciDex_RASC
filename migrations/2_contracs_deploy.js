0386948cc45d3b207e20eb6f165b33564278a4fa
Yaschenko <yura.yaschenko@gmail.com>
2018-08-09 10:21:37 +0300

    updated migrations

--- migrations/2_contracs_deploy.js
+++ migrations/2_contracs_deploy.js
@@ -1,5 +1,8 @@
-var Ownable = artifacts.require("./Ownable.sol");
+var ArrayUtils = artifacts.require("./ArrayUtils.sol");
+var StringUtils = artifacts.require("./StringUtils.sol");
 var SafeMath = artifacts.require("./SafeMath.sol");
+var Ownable = artifacts.require("./Ownable.sol");
+
 var RASC_Access = artifacts.require("./RASC_Access.sol");
 var RASC_UserFields = artifacts.require("./RASC_UserFields.sol");
 var RASC_User = artifacts.require("./RASC_User.sol");
@@ -11,12 +14,16 @@ var RASC_StoreTest = artifacts.require("./RASC_StoreTest.sol");
 module.exports = function(deployer, network) {
   deployer.deploy(Ownable);
   deployer.deploy(SafeMath);
+  deployer.deploy(StringUtils);
+  deployer.deploy(ArrayUtils);
+
   deployer.deploy(RASC_Access);
   deployer.deploy(RASC_UserFields);
   deployer.deploy(RASC_User);
   deployer.deploy(RASC_Item);
   deployer.deploy(RASC_Transaction);
   deployer.deploy(RASC_Store);
+  
   if (network == "local") {
     deployer.deploy(RASC_StoreTest);
   }
